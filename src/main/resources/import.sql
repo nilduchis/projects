a50b84c5b1aa90024a824182cc59ca38fc611eeb
Nilda <nildayatzyry@gmail.com>
2022-04-25 21:04:25 -0500

    spring-boot-project

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+insert into clientes (nombre, apellido, email, create_at) values ('Anddres', 'Guzman', 'profe@correo.com', '2022-03-21');
+insert into clientes (nombre, apellido, email, create_at) values ('Jossue', 'Loubet', 'jossue@correo.com', '2022-03-21');
+insert into clientes (nombre, apellido, email, create_at) values ('Nilda', 'Flores', 'nilda@correo.com', '2022-03-21');
